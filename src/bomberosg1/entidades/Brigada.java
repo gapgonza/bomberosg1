9ce2ac99f188f38fa2d0a05ff9f7056cc6bba874
MariaJulianaAguero <110269481+MariaJulianaAguero@users.noreply.github.com>
2023-10-26 16:45:44 -0300

    clase brigada

--- src/bomberosg1/entidades/Brigada.java
+++ src/bomberosg1/entidades/Brigada.java
@@ -10,4 +10,39 @@ public class Brigada {
     private String nombreClave;
     private Especialidad especialidad; // Esta puede ser una enumeración
     private List<Bombero> bomberos; // Una lista de bomberos
+
+    public Brigada() {
+    }
+
+    public Brigada(String nombreClave, Especialidad especialidad, List<Bombero> bomberos) {
+        this.nombreClave = nombreClave;
+        this.especialidad = especialidad;
+        this.bomberos = bomberos;
+    }
+
+    public String getNombreClave() {
+        return nombreClave;
+    }
+
+    public void setNombreClave(String nombreClave) {
+        this.nombreClave = nombreClave;
+    }
+
+    public Especialidad getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(Especialidad especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public List<Bombero> getBomberos() {
+        return bomberos;
+    }
+
+    public void setBomberos(List<Bombero> bomberos) {
+        this.bomberos = bomberos;
+    }
+    
+    
 }
