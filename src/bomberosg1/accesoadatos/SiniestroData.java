3e3cf9095d7ad1b533858bb0915b120299e3a3b1
ALAN PEÑIÑOREY <gapgonza@gmail.com>
2023-11-12 23:25:54 -0300

    Ingreso de Emergencia
    Increible, pero funciona

--- src/bomberosg1/accesoadatos/SiniestroData.java
+++ src/bomberosg1/accesoadatos/SiniestroData.java
@@ -6,6 +6,8 @@ package bomberosg1.accesoadatos;
 
 import bomberosg1.entidades.Siniestro;
 import java.sql.*;
+import javax.swing.JOptionPane;
+
 
 /**
  *
@@ -18,8 +20,29 @@ public class SiniestroData {
         con = Conexion.getConexion();
     }
     
-    public void agregarSiniestro(Siniestro siniestro){
-        String sql = "INSERT INTO `siniestro`(`tipo`, `fechaSiniestro`, `longitudX`, `latitudY`, `detalles`, `activo`, `horaSiniestro`)" 
+    public void agregarSiniestro(Siniestro siniestro) {
+        String sql = "INSERT INTO `siniestro`(`tipo`, `fechaSiniestro`, `longitudX`, `latitudY`, `detalles`, `activo`, `horaSiniestro`)"
                 + "VALUES(?,?,?,?,?,?,?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, siniestro.getTipo());
+            ps.setDate(2, Date.valueOf(siniestro.getFechaSiniestro()));
+            ps.setInt(3, siniestro.getLongitudX());
+            ps.setInt(4, siniestro.getLatitudY());
+            ps.setString(5, siniestro.getDetalles());
+            ps.setBoolean(6, siniestro.isActivo());
+            ps.setTime(7, Time.valueOf(siniestro.getHoraSiniestro()));
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                siniestro.setIdSiniestro(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "El siniestro fue agregado");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Siniestro");
+        }
+
     }
 }
